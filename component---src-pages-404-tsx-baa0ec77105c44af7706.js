f8f800b4c5591516ff4b2d09fc4597248b4244c7
Arun Murugan <arunmurugan200@gmail.com>
2021-04-20 10:59:57 +0530

    Updates

--- component---src-pages-404-tsx-baa0ec77105c44af7706.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"i6+/":function(t,e,n){"use strict";n.r(e);var a=n("hGi/"),o=n("q1tI"),c=n.n(o),i=n("Nhdc");n("t+Ps");e.default=function(t){return Object(a.a)(t),c.a.createElement(i.f,{title:"Page Not Found"},c.a.createElement("h1",{style:{padding:"70px"}},"404 - Page Not Found"))}},"t+Ps":function(t,e,n){}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-baa0ec77105c44af7706.js.map
